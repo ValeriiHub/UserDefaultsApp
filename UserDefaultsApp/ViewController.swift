35c7909e01794143afe2ae878096ca8b7c9ba960
ValeriiHub <p04taera@gmail.com>
2021-07-12 21:17:25 +0300

    Initial Commit

---
+++ UserDefaultsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsApp
+//
+//  Created by Valerii D on 12.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
